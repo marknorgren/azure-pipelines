0b07fef1859e4bf9ec85be9ffffa88d2cd77888a
Mark Norgren <mark@marknorgren.com>
2019-01-19 11:49:53 -0600

    Initial Commit

---
+++ azure-pipelines/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  azure-pipelines
+//
+//  Created by Mark Norgren on 1/19/19.
+//  Copyright © 2019 Marked Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ azure-pipelinesTests/azure_pipelinesTests.swift
@@ -0,0 +1,34 @@
+//
+//  azure_pipelinesTests.swift
+//  azure-pipelinesTests
+//
+//  Created by Mark Norgren on 1/19/19.
+//  Copyright © 2019 Marked Systems. All rights reserved.
+//
+
+import XCTest
+@testable import azure_pipelines
+
+class azure_pipelinesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
